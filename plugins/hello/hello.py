ba14e4b787abfbffa8f9fbc164e70f58103bc7a1
hyqskevin <hyqs_kevin@163.com>
2023-11-20 21:23:42 +0800

    update config

--- plugins/hello/hello.py
+++ plugins/hello/hello.py
@@ -41,16 +41,16 @@ class Hello(Plugin):
                 return
             e_context["context"].type = ContextType.TEXT
             msg: ChatMessage = e_context["context"]["msg"]
-            e_context["context"].content = f'请你随机使用一种风格说一句问候语来欢迎新用户"{msg.actual_user_nickname}"加入群聊。'
+            e_context["context"].content = f'请你随机使用一种风格说一句问候语来欢迎新用户"{msg.actual_user_nickname}"加入名字为“造作星期八”的群聊。'
             e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
             return
 
-        # if e_context["context"].type == ContextType.PATPAT:
+        if e_context["context"].type == ContextType.PATPAT:
         #     e_context["context"].type = ContextType.TEXT
         #     msg: ChatMessage = e_context["context"]["msg"]
         #     e_context["context"].content = f"请你随机使用一种风格介绍你自己，并告诉用户输入#help可以查看帮助信息。"
         #     e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
-        #     return
+            return
 
         content = e_context["context"].content
         logger.debug("[Hello] on_handle_context. content: %s" % content)
